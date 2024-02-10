c8b435e777e884b523e18490973c95bc04361a28
alexunderlong <157208506+alexunderlong@users.noreply.github.com>
2024-02-10 11:54:05 +0300

    Update offerskb.py

--- keyboards/offerskb.py
+++ keyboards/offerskb.py
@@ -14,22 +14,22 @@ def offers_kb(posts, n):
             offers_kb.row(cur)
 
     if 10 >= n >= len(posts):
-        cancel = InlineKeyboardButton(text="Выйти🔙", callback_data="market")
+        cancel = InlineKeyboardButton(text="Выйти 🔙", callback_data="market")
         offers_kb.row(cancel)
     elif n == 10:
-        forward = InlineKeyboardButton(text="Вперед➡️", callback_data="forward_offers")
-        cancel = InlineKeyboardButton(text="Выйти🔙", callback_data="market")
+        forward = InlineKeyboardButton(text="Вперед ➡️", callback_data="forward_offers")
+        cancel = InlineKeyboardButton(text="Выйти 🔙", callback_data="market")
         offers_kb.row(forward)
         offers_kb.row(cancel)
     elif n >= len(posts):
-        back = InlineKeyboardButton(text="Назад⬅️", callback_data="back_offers")
-        cancel = InlineKeyboardButton(text="Выйти🔙", callback_data="cancel_offers")
+        back = InlineKeyboardButton(text="Назад ⬅️", callback_data="back_offers")
+        cancel = InlineKeyboardButton(text="Выйти 🔙", callback_data="cancel_offers")
         offers_kb.row(back)
         offers_kb.row(cancel)
     else:
-        forward = InlineKeyboardButton(text="Вперед➡️", callback_data="forward_offers")
-        back = InlineKeyboardButton(text="Назад⬅️", callback_data="back_offers")
-        cancel = InlineKeyboardButton(text="Выйти🔙", callback_data="market")
+        forward = InlineKeyboardButton(text="Вперед ➡️", callback_data="forward_offers")
+        back = InlineKeyboardButton(text="Назад ⬅️", callback_data="back_offers")
+        cancel = InlineKeyboardButton(text="Выйти 🔙", callback_data="market")
         offers_kb.row(back, forward)
         offers_kb.row(cancel)
     return offers_kb.as_markup()
